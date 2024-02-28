96db9c14e1e28b355d25c98b6e0cd24da49705bd
Lucas <camargolimalucas@gmail.com>
2024-02-28 15:22:14 -0300

    pequenas melhorias

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../context/AuthContext';
 import { toastAlerta } from '../../util/toastAlerta';
+import { HouseSimple, PlusSquare, List, SignOut, User, GameController } from '@phosphor-icons/react';
 import './NavBar.css';
 
 function Navbar() {
@@ -23,12 +24,21 @@ function Navbar() {
         <div className="container flex justify-between text-lg">
           <Link to='/home' className='text-2xl font-bold uppercase'>Diário Pokémon</Link>
           <div className='flex gap-4 navbar-links'>
-            <Link to='/postagens' className=' hover:underline'>Postagens</Link>
-            <Link to='/temas' className=' hover:underline'>Temas</Link>
-            <Link to='/cadastroTema' className=' hover:underline'>Cadastrar tema</Link>
-            <Link to='/perfil' className=' hover:underline'>Perfil</Link>
-            <Link to='/pokedex' className=' hover:underline'>Pokédex</Link>
-            <Link to='' onClick={logout} className=' hover:underline'>Sair</Link>
+          
+            <HouseSimple size={18}></HouseSimple><Link to='/home' className='hover:underline'>Home</Link>
+
+            <List size={18}></List><Link to='/postagens' className=' hover:underline'>Postagens</Link>
+
+            <List size={18}></List><Link to='/temas' className=' hover:underline'>Temas</Link>
+
+            <PlusSquare size={18}></PlusSquare><Link to='/cadastroTema' className=' hover:underline'>Cadastrar tema</Link>
+
+            <User size={18}></User><Link to='/perfil' className=' hover:underline'>Perfil</Link>
+
+            <GameController size={18}></GameController><Link to='/pokedex' className=' hover:underline'>Pokédex</Link>
+
+            <SignOut size={18}></SignOut><Link to='' onClick={logout} className=' hover:underline'>Sair</Link>
+
           </div>
         </div>
       </div>
